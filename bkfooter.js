a9945dab144d25635b4007d837aa48042220b9f4
Ichung Young <ichung_young@yahoo.com>
2021-03-03 00:28:45 -0500

    change contact email to yahoo

--- bkfooter.js
+++ bkfooter.js
@@ -1,7 +1,7 @@
 Vue.component('bk-footer', {
     template: `<div class="navbar navbar-expand-sm navbar-custom m-3 bg-dark text-white">
     <ul class="navbar-nav mr-auto ml-auto">
-        <li class="nav-item"><a class="nav-link" href="mail:contact@bellakitchennj.com">contact@bellakitchennj.com</a></li>
+        <li class="nav-item"><a class="nav-link" href="mail:bellakitchennj@yahoo.com">bellakitchennj@yahoo.com</a></li>
         <li class="nav-separator"></li>
         <li class="nav-item"><a class="nav-link" href="tel:73276920">Store: 73276920</a></li>
         <li class="nav-separator"></li>
--- components/bkfooter.js
+++ components/bkfooter.js
@@ -14,13 +14,13 @@ Vue.component('bk-footer', {
         <span class="nav-link" >
         <a href="https://www.facebook.com/Bella-Kitchen-nj-110265187241600/"><i class="fab fa-facebook-f fa-lg" style="color: white;"></i></a>
         <a href="https://www.instagram.com/bella.kitchen.nj/"><i class="fab fa-instagram fa-lg" style="color: white; margin-left: 16px;"></i></a>
-        <a class="d-inline d-lg-none" href="mailto:contact@bellakitchennj.com"><i class="fas fa-envelope fa-lg" style="color: white; margin-left: 16px;"></i></a>
+        <a class="d-inline d-lg-none" href="mailto:bellakitchennj@yahoo.com"><i class="fas fa-envelope fa-lg" style="color: white; margin-left: 16px;"></i></a>
         <a class="d-inline d-lg-none" href="tel:7327697920"><i class="fas fa-phone-alt fa-lg" style="color: white; margin-left: 16px;"></i></a>
         <a class="d-inline d-lg-none" href="tel:7322163115"><i class="fas fa-mobile-alt fa-lg" style="color: white; margin-left: 16px;"></i></a>
         </span>
         </li>
         <li class="nav-separator d-none d-lg-inline"></li>
-        <li class="nav-item d-none d-lg-inline"><a class="nav-link" href="mailto:contact@bellakitchennj.com"><i class="fas fa-envelope fa-md" style="color: white;"></i> contact@bellakitchennj.com</a></li>
+        <li class="nav-item d-none d-lg-inline"><a class="nav-link" href="mailto:bellakitchennj@yahoo.com"><i class="fas fa-envelope fa-md" style="color: white;"></i>bellakitchennj@yahoo.com</a></li>
         <li class="nav-separator d-none d-lg-inline"></li>
         <li class="nav-item d-none d-lg-inline"><a class="nav-link" href="tel:7327697920"><i class="fas fa-phone-alt fa-md" style="color: white;"></i>  (732) 769-7920</a></li>
         <li class="nav-separator d-none d-lg-inline"></li>
